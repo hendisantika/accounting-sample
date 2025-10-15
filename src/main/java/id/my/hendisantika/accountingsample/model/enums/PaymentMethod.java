830cf3e53f682c24c4cd07523ffc05505565c6a1
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:42:19 +0700

    feat: Add PaymentMethod enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/model/enums/PaymentMethod.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.accountingsample.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 09.38
+ * To change this template use File | Settings | File Templates.
+ */
+public enum PaymentMethod {
+    CASH,
+    BANK_TRANSFER,
+    CREDIT_CARD,
+    DEBIT_CARD,
+    CHECK,
+    PAYPAL,
+    STRIPE,
+    OTHER
+}
