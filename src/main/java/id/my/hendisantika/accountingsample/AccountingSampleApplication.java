1649240ffc439179aa7ea3924ea392a0b3d93ab3
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 05:46:47 +0700

    feat: init project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/AccountingSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.accountingsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingSampleApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(AccountingSampleApplication.class, args);
+    }
+
+}
