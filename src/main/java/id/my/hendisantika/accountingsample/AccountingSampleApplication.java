23bca0b9ede35c1cc5804dbc79ac309aa913afac
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:24:46 +0700

    feat: Add required container in compose.yaml🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/accountingsample/AccountingSampleApplication.java
+++ src/main/java/id/my/hendisantika/accountingsample/AccountingSampleApplication.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.accountingsample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 08.00
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootApplication
 public class AccountingSampleApplication {
 
