ff20896d20d0d12aeada10a0480473b192fb2a2f
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:41:48 +0700

    feat: Add AccountType enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/model/enums/AccountType.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.accountingsample.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 09.36
+ * To change this template use File | Settings | File Templates.
+ */
+public enum AccountType {
+    ASSET,
+    LIABILITY,
+    EQUITY,
+    REVENUE,
+    EXPENSE,
+    COST_OF_GOODS_SOLD
+}
