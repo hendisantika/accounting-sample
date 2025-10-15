c15901e020bace6f75b8e69c94005083e307b0cc
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:51:18 +0700

    feat: Add ChangePasswordRequest DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/dto/user/ChangePasswordRequest.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.accountingsample.dto.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 11.40
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String newPassword;
+
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+}
