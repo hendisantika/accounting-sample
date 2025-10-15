d2eb2f1570f96a07634b3d4bfd1cdcfec8f93319
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:24:29 +0700

    feat: Add required configs🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/accountingsample/AccountingSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/accountingsample/AccountingSampleApplicationTests.java
@@ -3,6 +3,17 @@ package id.my.hendisantika.accountingsample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 08.00
+ * To change this template use File | Settings | File Templates.
+ */
 @SpringBootTest
 class AccountingSampleApplicationTests {
 
