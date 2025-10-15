fda7c2b8a927bf17b9fdcca50629f525f4e07963
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:51:52 +0700

    feat: Add BusinessException DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/exception/BusinessException.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.accountingsample.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 09.25
+ * To change this template use File | Settings | File Templates.
+ */
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
