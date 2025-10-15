fe2f5e68e82e7b60ed1a99d01fd7a1592c8b1eb7
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:48:00 +0700

    feat: Add LoginRequest DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/dto/auth/LoginRequest.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.accountingsample.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 10.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
