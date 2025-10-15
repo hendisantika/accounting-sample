7fee19a8c9d8c4190e0e7ee906372d4264904be9
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:42:32 +0700

    feat: Add UserRole enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/model/enums/UserRole.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.accountingsample.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 09.35
+ * To change this template use File | Settings | File Templates.
+ */
+public enum UserRole {
+    OWNER,
+    ADMIN,
+    ACCOUNTANT,
+    USER,
+    VIEWER
+}
