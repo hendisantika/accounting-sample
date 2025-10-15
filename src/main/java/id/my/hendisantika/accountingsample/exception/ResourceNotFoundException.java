3204aeedca7e8cc0041dc33f0bd29d3f9babe3e2
hendisantika <hendisantika@yahoo.co.id>
2025-10-16 06:52:11 +0700

    feat: Add ResourceNotFoundException DTO🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/exception/ResourceNotFoundException.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.accountingsample.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 09.20
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+    }
+}
