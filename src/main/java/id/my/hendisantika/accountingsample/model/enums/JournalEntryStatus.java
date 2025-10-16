2fda4944de6356a3086c8302cc2b2e3aac637cc9
hendisantika <hendisantika@yahoo.co.id>
2025-10-17 05:30:41 +0700

    feat: Add enums/JournalEntryStatus🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/model/enums/JournalEntryStatus.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.accountingsample.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 13.45
+ * To change this template use File | Settings | File Templates.
+ */
+public enum JournalEntryStatus {
+    DRAFT,
+    POSTED,
+    REVERSED
+}
