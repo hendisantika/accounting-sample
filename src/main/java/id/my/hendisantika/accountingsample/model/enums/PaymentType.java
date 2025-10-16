1e41e9bdc78b9cf7502a362bd9a560bf56ea2955
hendisantika <hendisantika@yahoo.co.id>
2025-10-17 05:30:53 +0700

    feat: Add enums/PaymentType🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/model/enums/PaymentType.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.accountingsample.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 13.40
+ * To change this template use File | Settings | File Templates.
+ */
+public enum PaymentType {
+    PAYMENT_RECEIVED,
+    PAYMENT_MADE
+}
