18a76869084c6f100192056d651b92ab914610dc
hendisantika <hendisantika@yahoo.co.id>
2025-10-17 05:30:29 +0700

    feat: Add enums/ItemType🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/model/enums/ItemType.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.accountingsample.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 13.25
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ItemType {
+    PRODUCT,
+    SERVICE,
+    GOODS
+}
