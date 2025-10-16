62a2609d44cadf4680a4eff9eeea564e2dfa2b43
hendisantika <hendisantika@yahoo.co.id>
2025-10-17 05:30:13 +0700

    feat: Add enums/BillStatus🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/accountingsample/model/enums/BillStatus.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.accountingsample.model.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : accounting-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 16/10/25
+ * Time: 13.35
+ * To change this template use File | Settings | File Templates.
+ */
+public enum BillStatus {
+    DRAFT,
+    SUBMITTED,
+    APPROVED,
+    PAID,
+    PARTIALLY_PAID,
+    OVERDUE,
+    CANCELLED
+}
